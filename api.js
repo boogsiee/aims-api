3aa30fb342b53ec40a5cec6831e4b107b59126d8
boogsiee <44293311+boogsiee@users.noreply.github.com>
2023-12-05 07:32:47 +0800

    server and db
    server and db

--- api.js
+++ api.js
@@ -3,8 +3,16 @@ const { uuid } = require("uuidv4");
 const router = express.Router();
 
 module.exports.setupRoutes = (db) => {
-  router.get("/users", (_, res) => {
-    db.all("SELECT * FROM Users", (err, rows) => {
+  router.get("/users", (req, res) => {
+    const { strand_name, section_number, batch_year } = req.query;
+
+    // Modify the SQL query accordingly to filter by strand_name, section_number, and batch_year
+    const sqlQuery = `
+      SELECT * FROM Users
+      WHERE section_number = ? AND batch_year = ?
+    `;
+
+    db.all(sqlQuery, [section_number, batch_year], (err, rows) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: "Internal Server Error" });
@@ -34,21 +42,43 @@ module.exports.setupRoutes = (db) => {
 
   router.post("/users", async (req, res) => {
     const {
-      userType,
+      userType = "Alumna",
       firstName,
       lastName,
       midName,
       suffix,
+      batch,
+      strand,
+      section,
       cnumber,
       username,
       pword,
       age,
     } = req.body;
+
+    console.log(req.body);
+
     const userId = uuid();
 
     try {
       await db.run(
-        "INSERT INTO Users (user_Id, user_type_role, user_fname, user_lname, user_mname, user_suffix, cnumber, username, password, age) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        `
+        INSERT INTO Users (
+          user_Id, 
+          user_type_role, 
+          user_fname, 
+          user_lname, 
+          user_mname, 
+          user_suffix, 
+          batch_year, 
+          strand_name, 
+          section_number, 
+          cnumber, 
+          username, 
+          password, 
+          age
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `,
         [
           userId,
           userType,
@@ -56,12 +86,16 @@ module.exports.setupRoutes = (db) => {
           lastName,
           midName,
           suffix,
+          batch,
+          strand,
+          section,
           cnumber,
           username,
           pword,
           age,
         ]
       );
+
       res.status(201).json({
         userId,
         userType,
@@ -69,13 +103,137 @@ module.exports.setupRoutes = (db) => {
         lastName,
         midName,
         suffix,
+        batch,
+        strand,
+        section,
         cnumber,
+        username,
         pword,
-        age,
       });
     } catch (error) {
       console.error(error);
-      res.status(500);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  router.get("/users/:userId", async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+      const getUserQuery = "SELECT * FROM Users WHERE user_Id = ?";
+      db.get(getUserQuery, [String(userId)], function (err, userData) {
+        if (err) {
+          console.error("Error fetching user data:", err);
+          return res.status(500).json({ error: "Internal server error" });
+        }
+
+        if (!userData) {
+          return res.status(404).json({ error: "User not found" });
+        }
+
+        const formattedUserData = {
+          user_ID: userData.user_Id,
+          user_type_role: userData.user_type_role,
+          user_fname: userData.user_fname,
+          user_lname: userData.user_lname,
+          user_mname: userData.user_mname,
+          batch_number: userData.batch_number,
+          strand_number: userData.strand_number,
+          section_number: userData.section_number,
+        };
+
+        res.status(200).json(formattedUserData);
+      });
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  router.put("/users/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const {
+      userType,
+      firstName,
+      lastName,
+      midName,
+      suffix,
+      batch,
+      strand,
+      section,
+      cnumber,
+      username,
+      pword,
+      age,
+    } = req.body;
+
+    try {
+      // Check if the user with the given ID exists
+      const existingUser = await db.get(
+        "SELECT * FROM Users WHERE user_ID = ?",
+        [userId]
+      );
+
+      if (!existingUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      // Perform the update
+      await db.run(
+        `
+        UPDATE Users
+        SET user_type_role = ?,
+            user_fname = ?,
+            user_lname = ?,
+            user_mname = ?,
+            user_suffix = ?,
+            batch_year = ?,
+            strand_name = ?,
+            section_number = ?,
+            cnumber = ?,
+            username = ?,
+            password = ?,
+            age = ?
+        WHERE user_ID = ?
+      `,
+        [
+          userType,
+          firstName,
+          lastName,
+          midName,
+          suffix,
+          batch,
+          strand,
+          section,
+          cnumber,
+          username,
+          pword,
+          age,
+          userId,
+        ]
+      );
+
+      // Fetch and return the updated user
+      const updatedUser = await db.get(
+        "SELECT * FROM Users WHERE user_ID = ?",
+        [userId]
+      );
+
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.delete("/users/:userId", async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+      await db.run("DELETE FROM Users WHERE user_ID = ?", [userId]);
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   });
 
@@ -90,7 +248,7 @@ module.exports.setupRoutes = (db) => {
     });
   });
 
-  router.get("/batch", (_, res) => {
+  router.get("/batch", (req, res) => {
     db.all("SELECT * FROM Batch", (err, rows) => {
       if (err) {
         console.error(err);
@@ -99,16 +257,33 @@ module.exports.setupRoutes = (db) => {
       }
       res
         .status(200)
-        .json({ data: rows || [], message: "Sucessfully fetched!" });
+        .json({ data: rows || [], message: "Successfully fetched!" });
+    });
+  });
+
+  router.get("/batch_years", (_, res) => {
+    db.all("SELECT DISTINCT batch_year FROM Batch", (err, rows) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      const batchYears = rows.map((row) => row.batch_year);
+      res.status(200).json({ batchYears });
     });
   });
 
   router.post("/batch", async (req, res) => {
-    const { year } = req.body;
+    const { bnumber, year, strand } = req.body;
     try {
-      await db.run("INSERT INTO Batch (year) VALUES (?)", [year]);
+      await db.run(
+        "INSERT INTO Batch (batch_number, batch_year, strand_number) VALUES (?, ?, ?)",
+        [bnumber, year, strand]
+      );
       res.status(201).json({
+        bnumber,
         year,
+        strand,
       });
     } catch (error) {
       console.error(error);
@@ -116,8 +291,8 @@ module.exports.setupRoutes = (db) => {
     }
   });
 
-  router.get("/posts", (_, res) => {
-    db.all("SELECT * FROM Posts", (err, rows) => {
+  router.get("/strands", (req, res) => {
+    db.all("SELECT * FROM Strand", (err, rows) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: "Internal Server Error" });
@@ -129,6 +304,30 @@ module.exports.setupRoutes = (db) => {
 
   router.get("/sections", (req, res) => {
     db.all("SELECT * FROM Sections", (err, rows) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      res.json(rows);
+    });
+  });
+
+  router.post("/strands", async (req, res) => {
+    const { strand } = req.body;
+    try {
+      await db.run("INSERT INTO Strand (strand_name) VALUES (?)", [strand]);
+      res.status(201).json({
+        strand,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.get("/posts", (_, res) => {
+    db.all("SELECT * FROM Posts", (err, rows) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: "Internal Server Error" });
@@ -151,17 +350,6 @@ module.exports.setupRoutes = (db) => {
       console.error(error);
       res.status(500);
     }
-  });
-
-  router.get("/strands", (req, res) => {
-    db.all("SELECT * FROM Strands", (err, rows) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal Server Error" });
-        return;
-      }
-      res.json(rows);
-    });
   });
 
   router.post("/strands", async (req, res) => {
--- server.js
+++ server.js
@@ -21,72 +21,72 @@ const isAuthenticated = (req, res, next) => {
 const db = new sqlite3.Database("aims-db.db");
 app.use(setupRoutes(db));
 
-app.post("/signup", (req, res) => {
-  const { username, password, user_type_role } = req.body;
-  const checkUserQuery = "SELECT * FROM Users WHERE username = ?";
-  db.get(checkUserQuery, [username], (err, existingUser) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "Internal server error" });
-      return;
-    }
-    if (existingUser) {
-      res.status(400).json({ error: "Username already taken" });
-    } else {
-      const insertUserQuery =
-        "INSERT INTO User (username, password, user_type_role) VALUES (?, ?, ?)";
-      db.run(insertUserQuery, [username, password, user_type_role], (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ error: "Internal server error" });
-          return;
-        }
-        res.status(201).json({ message: "User registered successfully" });
-      });
-    }
-  });
-});
+// app.post("/signup", (req, res) => {
+//   const { username, password, user_type_role } = req.body;
+//   const checkUserQuery = "SELECT * FROM Users WHERE username = ?";
+//   db.get(checkUserQuery, [username], (err, existingUser) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).json({ error: "Internal server error" });
+//       return;
+//     }
+//     if (existingUser) {
+//       res.status(400).json({ error: "Username already taken" });
+//     } else {
+//       const insertUserQuery =
+//         "INSERT INTO User (username, password, user_type_role) VALUES (?, ?, ?)";
+//       db.run(insertUserQuery, [username, password, user_type_role], (err) => {
+//         if (err) {
+//           console.error(err);
+//           res.status(500).json({ error: "Internal server error" });
+//           return;
+//         }
+//         res.status(201).json({ message: "User registered successfully" });
+//       });
+//     }
+//   });
+// });
 
-app.post("/", (req, res) => {
-  const { password, username } = req.body;
+// app.post("/", (req, res) => {
+//   const { password, username } = req.body;
 
-  const user = null;
+//   const user = null;
 
-  if (!user) {
-    res.status(401).json({ error: "Invalid credentials" });
-    return;
-  }
+//   if (!user) {
+//     res.status(401).json({ error: "Invalid credentials" });
+//     return;
+//   }
 
-  if (user.username !== username) {
-    res.status(401).json({ error: "Invalid credentials" });
-    return;
-  }
+//   if (user.username !== username) {
+//     res.status(401).json({ error: "Invalid credentials" });
+//     return;
+//   }
 
-  if (user.password !== password) {
-    res.status(401).json({ error: "Invalid credentials" });
-    return;
-  }
+//   if (user.password !== password) {
+//     res.status(401).json({ error: "Invalid credentials" });
+//     return;
+//   }
 
-  const userType = null;
+//   const userType = null;
 
-  if (userType && userType.user_type_role === "admin") {
-    // Admin user, grant access
-    res.json({ message: "Admin user authenticated", role: "admin" });
-  } else {
-    // Non-admin user, check if registered
-    if (user.registered) {
-      // Registered user, grant access
-      res.json({ message: "Registered user authenticated", role: "alumna" });
-    } else {
-      // Non-registered user
-      res.status(403).json({ error: "Access denied. User not registered" });
-    }
-  }
-});
-app.get("/home", isAuthenticated, (req, res) => {
-  // Only authenticated users will reach this point
-  res.json({ message: "Welcome to the home page!" });
-});
+//   if (userType && userType.user_type_role === "admin") {
+//     // Admin user, grant access
+//     res.json({ message: "Admin user authenticated", role: "admin" });
+//   } else {
+//     // Non-admin user, check if registered
+//     if (user.registered) {
+//       // Registered user, grant access
+//       res.json({ message: "Registered user authenticated", role: "alumna" });
+//     } else {
+//       // Non-registered user
+//       res.status(403).json({ error: "Access denied. User not registered" });
+//     }
+//   }
+// });
+// app.get("/home", isAuthenticated, (req, res) => {
+//   // Only authenticated users will reach this point
+//   res.json({ message: "Welcome to the home page!" });
+// });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
